5595869cb2593d32dff55fa0642782501a5d68a0
ceik <ceik@users.noreply.github.com>
2015-11-15 11:10:30 +0800

    work on stage 4

--- intro_prog/stage_4/submission/main.py
+++ intro_prog/stage_4/submission/main.py
@@ -1,10 +1,37 @@
 # Submission Stage 4
+# To run this: python "C:\Program Files (x86)\Google\google_appengine\dev_appserver.py" submission\
 
 import os
 import jinja2
 import webapp2
 
-# Set up jinja environment
+# import guestbook.py
+import cgi
+import urllib
+from google.appengine.api import users
+from google.appengine.ext import ndb
+
+DEFAULT_GUESTBOOK_NAME = 'default_guestbook'
+
+def guestbook_key(guestbook_name=DEFAULT_GUESTBOOK_NAME):
+    """Constructs a Datastore key for a Guestbook entity. We use guestbook_name
+    as the key."""
+    return ndb.Key('Guestbook', guestbook_name)
+
+class Author(ndb.Model):
+    """Sub model for representing an author."""
+    identity = ndb.StringProperty(indexed=False)
+    email = ndb.StringProperty(indexed=False)
+
+class Greeting(ndb.Model):
+    """A main model for representing an individual Guestbook entry."""
+    author = ndb.StructuredProperty(Author)
+    content = ndb.StringProperty(indexed=False)
+    date = ndb.DateTimeProperty(auto_now_add=True)
+
+
+
+
 
 template_dir = os.path.join(os.path.dirname(__file__), "templates")
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
@@ -21,12 +48,33 @@ class Handler(webapp2.RequestHandler):
     def render(self, template, **kw):
         self.write(self.render_str(template, **kw))
 
-class Handler(Handler):
-    def write(self, *a, **kw):
-        self.response.out.write(*a, **kw)
-
 class MainPage(Handler):
+    def get(self):
+        self.render("main.html")
+
+class Notes1Page(Handler):
     def get(self):
         self.render("notes_stage_1.html")
 
-app = webapp2.WSGIApplication([("/", MainPage)])
+class Notes4Page(Handler):
+    def get(self):
+        self.render("notes_stage_4.html")
+
+
+
+
+
+class GuestbookPage(Handler):
+    def get(self):
+        self.render("guestbook.html")
+
+
+
+
+
+app = webapp2.WSGIApplication([
+    ("/", MainPage),
+    ("/notes1", Notes1Page),
+    ("/notes4", Notes4Page),
+    ("/guestbook", GuestbookPage)
+])
